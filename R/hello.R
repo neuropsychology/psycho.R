a38973eff44ccdaeab193ac7e2c325b693e80b6d
DominiqueMakowski <dom.mak19@gmail.com>
2017-07-26 12:21:19 +0200

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,20 @@
+#' Illustration of Hello world
+#'
+#' prints hellow world \code{brocolors}
+#' Use `devtools::document()`
+#'devtools::build_vignettes()
+#'
+#'
+#' @param method2order method to order colors (\code{"hsv"} or \code{"cluster"})
+#' @param cex character expansion for the text
+#' @param mar margin paramaters; vector of length 4 (see \code{\link[graphics]{par}})
+#'
+#' @return None
+#'
+#' @examples
+#' hello()
+#'
+#' @export
+hello <- function(method2order, cex, mar) {
+  print("Hello, world!")
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# psycho
+**Under dev. See [neuropsychology](ttps://github.com/neuropsychology/neuropsychology.R)**
