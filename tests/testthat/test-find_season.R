5d226d9257181a4297e4f6f323dd5f0b1a830b06
DominiqueMakowski <dom.mak19@gmail.com>
2017-10-04 09:08:35 +0200

    tests

---
+++ tests/testthat/test-find_season.R
@@ -0,0 +1,7 @@
+context("find_season")
+
+test_that("Correct date", {
+  dates <- c("2017-02-15", "2017-05-15", "2017-08-15", "2017-11-15")
+  dates <- find_season(dates)
+  expect_equal(dates[1], "Winter")
+})
