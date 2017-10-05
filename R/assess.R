7f4816e8951c4516294f60c458d9c089c5305aef
Viliam Simko <viliam.simko@gmail.com>
2017-10-05 19:37:48 +0200

    Merge branch 'make_lintr_happy' into tomerge
    # Conflicts:
    #	R/analyze.stanreg.R
    #	R/correlation.R
    #	R/save_file.R
    #	inst/doc/ExampleVignette.R

--- R/assess.R
+++ R/assess.R
@@ -20,7 +20,12 @@
 #' @import ggplot2
 #' @import dplyr
 #' @export
-assess <- function(score, mean=0, sd=1, linecolor="#E91E63", fillcolor="#2196F3", verbose=T) {
+assess <- function(score,
+                   mean = 0,
+                   sd = 1,
+                   linecolor = "#E91E63",
+                   fillcolor = "#2196F3",
+                   verbose = T) {
 
   # Values
   # -------------
@@ -28,24 +33,24 @@ assess <- function(score, mean=0, sd=1, linecolor="#E91E63", fillcolor="#2196F3"
   values$psycho_function <- "assess"
   values$psycho_name <- deparse(score)
 
-  values$distribution <- stats::rnorm(50000, mean=mean, sd=sd)
+  values$distribution <- stats::rnorm(50000, mean = mean, sd = sd)
   values$percentile <- stats::ecdf(values$distribution)(score)
-  values$z_score <- (score-mean)/sd
+  values$z_score <- (score - mean) / sd
 
   # Summary
   # -------------
-  summary <- data.frame(Score=score)
+  summary <- data.frame(Score = score)
   summary$Function <- values$psycho_function
   summary$Percentile <- values$percentile
   summary$Z_Score <- values$z_score
 
 
   # If score is list
-  if (length(score) > 1){
-    if (verbose == T){
-      warning('Multiple scores were provided. Returning a list containing summmary and values.')
+  if (length(score) > 1) {
+    if (verbose == T) {
+      warning("Multiple scores were provided. Returning a list containing summmary and values.")
     }
-    output <- list(summary=summary, values=values)
+    output <- list(summary = summary, values = values)
     return(output)
   }
 
@@ -59,31 +64,36 @@ assess <- function(score, mean=0, sd=1, linecolor="#E91E63", fillcolor="#2196F3"
     comparison <- "greater"
   }
 
-  text <- paste("The participant (score = ",
-                score,
-                ") is positioned at ",
-                as.character(round((score-mean)/sd, 2)),
-                " standard deviations from the mean (M = ",
-                as.character(mean),
-                ", SD = ",
-                as.character(sd),
-                "). ",
-                "The participant's score is ",
-                comparison,
-                " than ",
-                as.character((round(values$percentile*100, 2))),
-                " % of the general population.", sep="")
+  text <- paste(
+    "The participant (score = ",
+    score,
+    ") is positioned at ",
+    as.character(round((score - mean) / sd, 2)),
+    " standard deviations from the mean (M = ",
+    as.character(mean),
+    ", SD = ",
+    as.character(sd),
+    "). ",
+    "The participant's score is ",
+    comparison,
+    " than ",
+    as.character((round(
+      values$percentile * 100, 2
+    ))),
+    " % of the general population.",
+    sep = ""
+  )
 
   # Plot
   # -------------
-  plot <- data.frame(Distribution=values$distribution) %>%
-    ggplot(aes_string(x="Distribution")) +
-    geom_density(fill=fillcolor, colour="white", adjust = 3, na.rm=TRUE) +
-    geom_vline(xintercept=score, size = 2, color=linecolor) +
+  plot <- data.frame(Distribution = values$distribution) %>%
+    ggplot(aes_string(x = "Distribution")) +
+    geom_density(fill = fillcolor, colour = "white", adjust = 3, na.rm = TRUE) +
+    geom_vline(xintercept = score, size = 2, color = linecolor) +
     xlab("") +
     ylab("")
 
-  output <- list(text=text, plot=plot, summary=summary, values=values)
+  output <- list(text = text, plot = plot, summary = summary, values = values)
 
   class(output) <- c("psychobject", "list")
   return(output)
--- tests/testthat/test-plot.psychobject.R
+++ tests/testthat/test-plot.psychobject.R
@@ -1,7 +1,7 @@
 context("plot.psychobject")
 
 test_that("It works", {
-  output <- list(plot=1)
+  output <- list(plot = 1)
   class(output) <- c("psychobject", "list")
   plot <- plot(output)
   expect_equal(plot, 1)
