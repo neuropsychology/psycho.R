28e1bcc78c2222127d4b53cde4082a1b2b78b960
DominiqueMakowski <dom.mak19@gmail.com>
2017-12-07 14:20:30 +0100

    testing for HDI

---
+++ tests/testthat/test-hdi.R
@@ -0,0 +1,10 @@
+context("hdi")
+
+test_that("Correct Value", {
+  x <- attitude$rating
+  results <- psycho::hdi(x, 0.95)
+
+  testthat::expect_equal(results$values$HDImin, 40)
+  testthat::expect_equal(length(plot(results)), 9)
+
+})
