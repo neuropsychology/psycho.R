968c9ff682923694ca759184310a2d1fb85e24e8
DominiqueMakowski <dom.mak19@gmail.com>
2018-03-24 17:32:44 +0100

    improve testing

--- tests/testthat/test-crawford.test.R
+++ tests/testthat/test-crawford.test.R
@@ -16,4 +16,12 @@ test_that("Correct Value", {
   )
 
   testthat::expect_equal(x$t, -3.3, tol = 0.2)
+
+  x <- crawford.test(
+    case = 0,
+    controls = c(0, -2, 5, 2, 1, 3, -4, -2),
+    verbose = T
+  )
+
+  testthat::expect_equal(x$t, -0.12, tol = 0.2)
 })
