4bc438fb0292d627a6f2a5d24cb7558c06bb6a51
Dominique Makowski <dom.mak19@gmail.com>
2018-03-30 16:54:58 +0200

    enhance mellenbergh

--- R/mellenbergh.test.R
+++ R/mellenbergh.test.R
@@ -11,6 +11,7 @@
 #'
 #' @examples
 #' mellenbergh.test(t0 = 4, t1 = 12, controls = c(0, -2, 5, 2, 1, 3, -4, -2))
+#' mellenbergh.test(t0 = 8, t1 = 2, controls = 2.6)
 #'
 #' @author Dominique Makowski
 #'
@@ -31,6 +32,17 @@ mellenbergh.test <- function(t0, t1, controls, verbose=T) {
   z <- diff / sd
   pval <- 2 * pnorm(-abs(z))
 
+  # One-tailed p value
+  if(pval > .05 & pval/2 < .05){
+    one_tailed <- paste0(" However, the null hypothesis of no change can be rejected at a one-tailed 5% significance level (one-tailed p ",
+                         format_p(pval/2),
+                         ").")
+
+  } else {
+    one_tailed <- ""
+  }
+
+
 
   p_interpretation <- ifelse(pval < 0.05, " ", " not ")
   text <- paste0(
@@ -46,8 +58,10 @@ mellenbergh.test <- function(t0, t1, controls, verbose=T) {
     format_digit(z),
     ", p ",
     format_p(pval),
-    ")."
+    ").",
+    one_tailed
   )
+
 
   result <- data.frame(diff = diff, diff_90_CI_lower = diff_CI_bottom, diff_90_CI_higher = diff_CI_top, z = z, p = pval)
 
--- tests/testthat/test-mellenbergh.test.R
+++ tests/testthat/test-mellenbergh.test.R
@@ -17,4 +17,9 @@ test_that("Correct Value", {
   )
 
   testthat::expect_equal(x$z, 2.22, tol = 0.2)
+
+  x <- mellenbergh.test(t0 = 4, t1 = 12, controls = c(0, -2, 5, 2, 1, 3, -4, -2))
+  testthat::expect_equal(x$z, 1.90, tol = 0.1)
+  x <- mellenbergh.test(t0 = 8, t1 = 2, controls = 2.6)
+  testthat::expect_equal(x$z, -1.63, tol = 0.1)
 })
