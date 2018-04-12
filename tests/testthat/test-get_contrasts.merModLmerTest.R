d45832777aa6d4d5de9d05c51a825a4150ff416d
DominiqueMakowski <dom.mak19@gmail.com>
2018-04-12 23:54:49 +0200

    Revert "remove merModLmerTest"
    This reverts commit 531f2ea1efa31b1c555bdb8fe4996bd4e5256076.

---
+++ tests/testthat/test-get_contrasts.merModLmerTest.R
@@ -0,0 +1,15 @@
+context("get_contrasts.merModLmerTest")
+
+test_that("If it works.", {
+  # Fit
+  require(lmerTest)
+
+  fit <- lmerTest::lmer(Adjusting ~ Birth_Season + (1 | Salary), data = psycho::affective)
+
+  contrasts <- psycho::get_contrasts(fit, formula = "Birth_Season")
+  means <- contrasts$means
+  contrasts <- contrasts$contrasts
+
+  testthat::expect_equal(mean(means$Mean), 3.860, tolerance = 0.05)
+  testthat::expect_equal(mean(contrasts$Difference), -0.218, tolerance = 0.05)
+})
