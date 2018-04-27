4009ad08386fef18a7b6ce2e9b447c820b3848ad
DominiqueMakowski <dom.mak19@gmail.com>
2018-04-27 21:25:42 +0200

    fix documentation

--- R/get_contrasts.lmerModLmerTest.R
+++ R/get_contrasts.lmerModLmerTest.R
@@ -6,7 +6,7 @@
 #' @param formula A character vector (formula like format, i.e., including
 #' interactions or nesting terms) specifying the names of the predictors over which EMMs are desired.
 #' @param adjust P value adjustment method. Default is "tukey". Can be "holm",
-#' "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none".
+#' "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr" or "none".
 #' @param ... Arguments passed to or from other methods.
 #'
 #'
