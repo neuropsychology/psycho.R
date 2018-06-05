1ee1b5cb7f2031c232753734b489e89f852bb497
DominiqueMakowski <dom.mak19@gmail.com>
2018-06-05 08:22:47 +0200

    testing

---
+++ tests/testthat/test-analyze.htest.R
@@ -0,0 +1,20 @@
+context("analyze.htest")
+
+test_that("If it works.", {
+  library(psycho)
+
+  df <- psycho::affective
+
+  x <- t.test(df$Adjusting, df$Concealing)
+  rez <- psycho::analyze(x)
+  testthat::expect_equal(ncol(summary(rez)), 6)
+
+  x <- cor.test(df$Adjusting, df$Concealing)
+  rez <- psycho::analyze(x)
+  testthat::expect_equal(ncol(summary(rez)), 6)
+
+  x <- t.test(df$Adjusting ~ df$Sex)
+  rez <- psycho::analyze(x)
+  testthat::expect_equal(ncol(summary(rez)), 6)
+
+  })
