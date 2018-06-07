cb1323b816589d3f487c3ea3f3d16eaa45298c5e
DominiqueMakowski <dom.mak19@gmail.com>
2018-06-07 13:57:41 +0200

    hotfix

--- tests/testthat/test-analyze.lavaan.R
+++ tests/testthat/test-analyze.lavaan.R
@@ -6,7 +6,7 @@ test_that("If it works.", {
 
   HS.model <- " visual  =~ x1 + x2 + x3\n  textual =~ x4 + x5 + x6\n  speed   =~ x7 + x8 + x9 "
 
-  fit <- cfa(HS.model, data = HolzingerSwineford1939)
-  rez <- analyze(x)
+  fit <- lavaan::cfa(HS.model, data = lavaan::HolzingerSwineford1939)
+  rez <- analyze(fit)
   testthat::expect_equal(nrow(summary(rez)), 24)
 })
