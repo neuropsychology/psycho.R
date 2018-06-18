a5499d4ac263c94e4353551cd68587e6173cd16b
Dominique Makowski <dom.mak19@gmail.com>
2018-06-18 19:15:04 +0200

    fix testing

--- tests/testthat/test-get_R2.R
+++ tests/testthat/test-get_R2.R
@@ -5,5 +5,5 @@ test_that("If it works.", {
   library(psycho)
 
   fit <- lm(Tolerating ~ Adjusting, data=psycho::affective)
-  testthat::expect_equal(get_R2(fit))
+  testthat::expect_equal(get_R2(fit)$R2, 0.08, tol=0.01)
 })
