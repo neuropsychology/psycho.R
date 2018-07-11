75f8bb06cd07ba3bdad0336f191192c210f9d597
DominiqueMakowski <dom.mak19@gmail.com>
2018-07-11 22:40:47 +0200

    fix testing

--- tests/testthat/test-analyze.aov.R
+++ tests/testthat/test-analyze.aov.R
@@ -2,20 +2,22 @@ context("analyze.aov")
 
 test_that("If it works.", {
   library(psycho)
+  library(lmerTest)
+  library(lme4)
 
   df <- psycho::affective
-  x <- aov(df$Tolerating ~ df$Salary)
+  x <- aov(Tolerating ~ Salary, data=df)
   testthat::expect_equal(nrow(summary(psycho::analyze(x))), 2)
 
-  x <- anova(lm(df$Tolerating ~ df$Salary))
+  x <- anova(lm(Tolerating ~ Salary, data=df))
   testthat::expect_equal(nrow(summary(psycho::analyze(x))), 2)
 
-  x <- aov(df$Tolerating ~ df$Birth_Season + Error(df$Sex))
-  testthat::expect_equal(nrow(summary(psycho::analyze(x))), 2)
+  x <- aov(Tolerating ~ Birth_Season + Error(Sex), data=df)
+  testthat::expect_message(psycho::analyze(x))
 
-  x <- anova(lmerTest::lmer(df$Tolerating ~ df$Birth_Season + (1|df$Sex)))
+  x <- anova(lmerTest::lmer(Tolerating ~ Birth_Season + (1|Sex), data=df))
   testthat::expect_equal(nrow(summary(psycho::analyze(x))), 1)
 
-  x <- anova(lme4::lmer(df$Tolerating ~ df$Birth_Season + (1|df$Sex)))
+  x <- anova(lme4::lmer(Tolerating ~ Birth_Season + (1|Sex), data=df))
   testthat::expect_error(psycho::analyze(x))
 })
