a48f8e046b558a480acfe6b984cdbb8da3ec9554
DominiqueMakowski <dom.mak19@gmail.com>
2018-07-11 19:51:53 +0200

    testing

--- tests/testthat/test-analyze.aov.R
+++ tests/testthat/test-analyze.aov.R
@@ -12,4 +12,10 @@ test_that("If it works.", {
 
   x <- aov(df$Tolerating ~ df$Birth_Season + Error(df$Sex))
   testthat::expect_equal(nrow(summary(psycho::analyze(x))), 2)
+
+  x <- anova(lmerTest::lmer(df$Tolerating ~ df$Birth_Season + (1|df$Sex)))
+  testthat::expect_equal(nrow(summary(psycho::analyze(x))), 1)
+
+  x <- anova(lme4::lmer(df$Tolerating ~ df$Birth_Season + (1|df$Sex)))
+  testthat::expect_error(psycho::analyze(x))
 })
