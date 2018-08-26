d2cbf1c7cd2bd0da7d832866a0289f854bc770a9
DominiqueMakowski <dom.mak19@gmail.com>
2018-08-26 14:09:49 +0200

    get analyze.stanreg to match preprint

--- R/simulate.R
+++ R/simulate.R
@@ -9,7 +9,7 @@
 #' @examples
 #' library(psycho)
 #'
-#' data <- simulate_data_regression(coefs=c(0, 0.8), sample=50, error=0)
+#' data <- simulate_data_regression(coefs=c(0.1, 0.8), sample=50, error=0)
 #' fit <- lm(y ~ ., data=data)
 #' coef(fit)
 #' analyze(fit)
