d7a02f93dcb96922261a92c1d7e06f0720230b89
Dominique Makowski <dom.mak19@gmail.com>
2020-01-22 11:23:56 +0800

    go back to 0.5.0

---
+++ vignettes/overview.R
@@ -0,0 +1,18 @@
+## ---- echo=F, message=FALSE, warning=FALSE------------------------------------
+library(knitr)
+
+## ---- out.width=700, echo = FALSE, eval = TRUE, fig.align='center'------------
+knitr::include_graphics("images/workflow.PNG")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  # This for the stable version:
+#  install.packages("psycho")
+#  
+#  # Or this for the dev version:
+#  install.packages("devtools")
+#  library(devtools)
+#  devtools::install_github("neuropsychology/psycho.R")
+
+## -----------------------------------------------------------------------------
+library(psycho)
+
